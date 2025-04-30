755f9c2374f1fa26c8dd5199ec9e2eb863fac2e0
stiles3 <48661880+stiles3@users.noreply.github.com>
2025-04-30 22:07:10 +0100

    cors origin modified

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envConfig } from './core/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // Explicitly allow frontend origin
+    origin: envConfig.FRONTEND_URL, // Explicitly allow frontend origin
     credentials: true, // Required for cookies
   });
   await app.listen(process.env.PORT ?? 3005);
